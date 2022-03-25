b3da8da1e5d729ce4161cc354c1c43d82c5a9bc1
PotionLeMotion <andrerandmaa2@gmail.com>
2022-03-25 23:04:11 +0200

    MoodustaSõna on täiendatud. Katki, erroreid palju

--- src/AlustaMängu.java
+++ src/AlustaMängu.java
@@ -2,13 +2,20 @@ import java.util.*;
 
 public class AlustaMängu {
 
+    /**
+     * shuffle kasutusele panna
+     * arvutused MoodustaSõnas korda teha
+     * MoodustaSõnas korda teha vigane tähtede sisestuse pool
+     * osa koodist mainist viia TähedKäes
+     */
+
     public static void main(String[] args) {
 
         String[] tähestik = {"A-1", "E-1", "I-1", "O-1", "U-1",
                 "L-1", "N-1", "S-1", "T-1", "R-1",
                 "D-2", "G-2", "B-3", "C-3", "M-3",
                 "P-3", "F-4", "H-4", "V-4", "W-4",
-                "Y-4", "K-5", "J-8", "X-8", "Q-10", "Z-10"};
+                "Y-4", "K-5", "J-8", "X-8", "Q-9", "Z-9"};
 
 
         ArrayList<String> mängija1Tähed = new ArrayList<>(TähedKäes.getTähtedeArv());
@@ -20,12 +27,12 @@ public class AlustaMängu {
         String vastavTäht;
 
         for (int i = 0; i < TähedKäes.getTähtedeArv(); i++) {
-            indeksTähestik = (int)(Math.random()*tähestikuPikkus);
+            indeksTähestik = (int) (Math.random() * tähestikuPikkus);
             vastavTäht = tähestik[indeksTähestik];
             mängija1Tähed.add(vastavTäht);
         }
         for (int i = 0; i < TähedKäes.getTähtedeArv(); i++) {
-            indeksTähestik = (int)(Math.random()*tähestikuPikkus);
+            indeksTähestik = (int) (Math.random() * tähestikuPikkus);
             vastavTäht = tähestik[indeksTähestik];
             mängija2Tähed.add(vastavTäht);
         }
@@ -40,7 +47,7 @@ public class AlustaMängu {
         Shuffle shuffle = new Shuffle(käsi, tähestik);
         System.out.println(shuffle.lisaSuvalisedTähed());
         System.out.println();
-
+/*
         punktisumma1.tähedPunktidKokku();
         punktisumma2.tähedPunktidKokku();
         System.out.println(punktisumma1.arvutaSkoor("WOW"));
@@ -55,62 +62,80 @@ public class AlustaMängu {
         System.out.println(punktisumma1.arvutaSkoor("HERO"));
         System.out.println(punktisumma1.getHetkeSkoor());
 
+
+ */
         //System.out.println(mängija1Käsi);
         //System.out.println(mängija2Käsi);
 
-
+        //String salvestaMängija1Nimi = "";
+        //String salvestaMängija2Nimi = "";
 
         System.out.println();
         System.out.println("Scrabble (meie moodi)");
         System.out.println();
+
         Scanner sisestusMängija1 = new Scanner(System.in);
         System.out.print("Mängija 1, sisesta oma nimi: ");
         String Mängija1 = sisestusMängija1.nextLine();
+        //salvestaMängija1Nimi = Mängija1;
         System.out.println("Sinu nimi on: " + Mängija1);
 
         Scanner sisestusMängija2 = new Scanner(System.in);
         System.out.print("Mängija 2, sisesta oma nimi: ");
         String Mängija2 = sisestusMängija2.nextLine();
+        //salvestaMängija2Nimi = Mängija2;
         System.out.println("Sinu nimi on: " + Mängija2);
 
-        //while loop, punktid 30
-        System.out.println("Mängija " + Mängija1 + " käik.");
-        System.out.println(mängija1Käsi);
-        System.out.print("Sisesta käesolevatest tähtedest sõna: ");
+        while (punktisumma1.getHetkeSkoor() < 20 || punktisumma2.getHetkeSkoor() < 20) {
 
-        Scanner mängija1SõnaSisestus = new Scanner(System.in);
-        String mängija1Sõna = mängija1SõnaSisestus.nextLine();
-        mängija1Sõna = mängija1Sõna.toUpperCase();
+            //while loop, punktid 30
+            System.out.println();
+            System.out.println("Mängija " + Mängija1 + " käik.");
+            System.out.println(mängija1Käsi);
+            System.out.print("Sisesta käesolevatest tähtedest sõna: ");
 
-        MoodustaSõna moodustaSõna1 = new MoodustaSõna(mängija1Tähed, mängija1Sõna);
-        moodustaSõna1.MoodustaTähtedestSõna(mängija1Tähed, mängija1Sõna);
-        System.out.println();
+            Scanner mängija1SõnaSisestus = new Scanner(System.in);
+            String mängija1Sõna = mängija1SõnaSisestus.nextLine();
+            mängija1Sõna = mängija1Sõna.toUpperCase();
 
-        System.out.println("Kas " + Mängija2 + " on antud sõnaga nõus? (y/n)");
-        Scanner jahVõiEi1 = new Scanner(System.in);
-        String vastus1 = jahVõiEi1.nextLine();
-        vastus1 = vastus1.toUpperCase();
-        moodustaSõna1.kasOnNõus(vastus1); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
+            MoodustaSõna moodustaSõna1 = new MoodustaSõna(mängija1Tähed, mängija1Sõna);
+            System.out.println();
 
-        System.out.println();
-        System.out.println("Mängija " + Mängija2 + " käik.");
-        System.out.println(mängija2Käsi);
-        System.out.print("Sisesta käesolevatest tähtedest sõna: ");
+            if (moodustaSõna1.MoodustaTähtedestSõna(mängija1Tähed, mängija1Sõna, 1) == true) {
+                System.out.println("Kas " + Mängija2 + " on antud sõnaga nõus? (y/n)");
+                Scanner jahVõiEi1 = new Scanner(System.in);
+                String vastus1 = jahVõiEi1.nextLine();
+                vastus1 = vastus1.toUpperCase();
+                moodustaSõna1.kasOnNõus(vastus1); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
+            }
 
-        Scanner mängija2SõnaSisestus = new Scanner(System.in);
-        String mängija2Sõna = mängija2SõnaSisestus.nextLine();
-        mängija2Sõna = mängija2Sõna.toUpperCase();
+            System.out.println();
+            System.out.println("Mängija " + Mängija2 + " käik.");
+            System.out.println(mängija2Käsi);
+            System.out.print("Sisesta käesolevatest tähtedest sõna: ");
 
-        MoodustaSõna moodustaSõna2 = new MoodustaSõna(mängija2Tähed, mängija2Sõna);
-        moodustaSõna2.MoodustaTähtedestSõna(mängija2Tähed, mängija2Sõna);
-        System.out.println();
+            Scanner mängija2SõnaSisestus = new Scanner(System.in);
+            String mängija2Sõna = mängija2SõnaSisestus.nextLine();
+            mängija2Sõna = mängija2Sõna.toUpperCase();
 
-        System.out.println("Kas " + Mängija1 + " on antud sõnaga nõus? (y/n)");
-        Scanner jahVõiEi2 = new Scanner(System.in);
-        String vastus2 = jahVõiEi2.nextLine();
-        vastus2 = vastus2.toUpperCase();
-        moodustaSõna1.kasOnNõus(vastus2); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
+            MoodustaSõna moodustaSõna2 = new MoodustaSõna(mängija2Tähed, mängija2Sõna);
+            System.out.println();
 
+            if (moodustaSõna2.MoodustaTähtedestSõna(mängija2Tähed, mängija2Sõna, 2) == true) {
+                System.out.println("Kas " + Mängija1 + " on antud sõnaga nõus? (y/n)");
+                Scanner jahVõiEi2 = new Scanner(System.in);
+                String vastus2 = jahVõiEi2.nextLine();
+                vastus2 = vastus2.toUpperCase();
+                moodustaSõna1.kasOnNõus(vastus2); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
+            }
+
+        }
+        if (punktisumma1.getHetkeSkoor() > punktisumma2.getHetkeSkoor()) {
+            System.out.println("Võitis " + Mängija1 + "!");
+            System.out.println("Punktisummaks jäi " + punktisumma1.getHetkeSkoor());
+        }
+        System.out.println("Võitis " + Mängija2 + "!");
+        System.out.println("Punktisummaks jäi " + punktisumma2.getHetkeSkoor());
     }
 
 }
--- src/MoodustaSõna.java
+++ src/MoodustaSõna.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Scanner;
 
 public class MoodustaSõna {
+
+    String[] tähestik = {"A-1", "E-1", "I-1", "O-1", "U-1",
+            "L-1", "N-1", "S-1", "T-1", "R-1",
+            "D-2", "G-2", "B-3", "C-3", "M-3",
+            "P-3", "F-4", "H-4", "V-4", "W-4",
+            "Y-4", "K-5", "J-8", "X-8", "Q-9", "Z-9"};
 
     private ArrayList<String> tähedKäes;
     private String sisestatudSõna;
@@ -11,22 +19,95 @@ public class MoodustaSõna {
         this.sisestatudSõna = sisestatudSõna;
     }
 
-    public void MoodustaTähtedestSõna(ArrayList<String> tähedKäes, String sisestatudSõna) {
-        ArrayList<String> tähedKäesIlmaNumbriteta = new ArrayList<>(tähedKäes);
+    public boolean MoodustaTähtedestSõna(ArrayList<String> tähedKäes, String sisestatudSõna, int mängija) {
+        boolean väljumineMeetodist = false;
+        ArrayList<String> tähedKäesIlmaNumbriteta = new ArrayList<>();
+        int kasTehaUuestiRing = 0;
+        String sisestusUuesti = "";
+        String salvestatudTähed2 = "";
+        String hinnatavSõna = "";
+
+
         for (String täht : tähedKäes) {
             tähedKäesIlmaNumbriteta.add(täht.substring(0,1));
         }
-        for (int i = 0; i < sisestatudSõna.length(); i++) {
-            if (tähedKäesIlmaNumbriteta.contains(sisestatudSõna.substring(i, i+1))) {
-                tähedKäesIlmaNumbriteta.remove(sisestatudSõna.substring(i, i+1));
-                if (i == sisestatudSõna.length() - 1)
-                    System.out.println("Sõna on võimalik moodustada!");
+        for (int i = 0; ; i++) {
+            if (kasTehaUuestiRing >= 1) {
+                if (kasTehaUuestiRing == 1)
+                i = 0;
+                String vastavTäht = sisestusUuesti.substring(i, i + 1);
+                if (tähedKäesIlmaNumbriteta.contains(vastavTäht)) {
+                    salvestatudTähed2 += vastavTäht;
+                    int täheleVastavIndeks = tähedKäesIlmaNumbriteta.indexOf(vastavTäht);
+                    tähedKäesIlmaNumbriteta.set(täheleVastavIndeks, "-");
+                    System.out.println(tähedKäesIlmaNumbriteta);
+                    if (i == sisestusUuesti.length() - 1) {
+                        System.out.println("Sõna on võimalik moodustada!");
+                        väljumineMeetodist = true;
+                        hinnatavSõna = sisestusUuesti;
+                        break;
+                    }
+                }
+            }
+            if (i < sisestatudSõna.length()) {
+                if (kasTehaUuestiRing == 0) {
+                    if (tähedKäesIlmaNumbriteta.contains(sisestatudSõna.substring(i, i + 1))) {
+                        String vastavTäht = sisestatudSõna.substring(i, i + 1);
+                        salvestatudTähed2 += vastavTäht;
+                        int täheleVastavIndeks = tähedKäesIlmaNumbriteta.indexOf(vastavTäht);
+                        tähedKäesIlmaNumbriteta.set(täheleVastavIndeks, "-");
+                        System.out.println(tähedKäesIlmaNumbriteta);
+                        if (i == sisestatudSõna.length() - 1) {
+                            System.out.println("Sõna on võimalik moodustada!");
+                            väljumineMeetodist = true;
+                            hinnatavSõna = sisestatudSõna;
+                            break;
+                        }
+                    }
+                }
             }
             else {
                 System.out.println("Sinu käesolevatest tähtedest pole võimalik antud sõna moodustada.");
-                break;
+                System.out.println();
+                System.out.println("Sinu tähed on: " + tähedKäes);
+                System.out.print("Kas oled nõus uuesti proovima? (Y/N) : ");
+                Scanner uusSisestus = new Scanner(System.in);
+                String Sisestus = uusSisestus.nextLine();
+                Sisestus = Sisestus.toUpperCase();
+                if (kasOnNõus(Sisestus) == false) {
+                    väljumineMeetodist = false;
+                    break;
+                }
+                else {
+                    System.out.println(tähedKäesIlmaNumbriteta);
+                    for (int j = 0; j < salvestatudTähed2.length(); j++) {
+                        int leiaIndeks = tähedKäesIlmaNumbriteta.indexOf("-");
+                        tähedKäesIlmaNumbriteta.set(leiaIndeks, salvestatudTähed2.substring(j, j+1));
+                    }
+                    salvestatudTähed2 = "";
+                    System.out.println(tähedKäesIlmaNumbriteta);
+                    kasTehaUuestiRing = 1;
+                    System.out.print("Sisesta käesolevatest tähtedest sõna: ");
+                    Scanner sisestatudSõnaUuesti = new Scanner(System.in);
+                    sisestusUuesti = sisestatudSõnaUuesti.nextLine();
+                    sisestusUuesti = sisestusUuesti.toUpperCase();
+                }
             }
         }
+        if (väljumineMeetodist == true) {
+            if (mängija == 1) {
+                Punktisumma punktisumma1 = new Punktisumma(new Hashtable<>(), tähestik, 0);
+                System.out.println(punktisumma1.arvutaSkoor(hinnatavSõna));
+                System.out.println(punktisumma1.getHetkeSkoor());
+            }
+            if (mängija == 2) {
+                Punktisumma punktisumma2 = new Punktisumma(new Hashtable<>(), tähestik, 0);
+                System.out.println(punktisumma2.arvutaSkoor(hinnatavSõna));
+                System.out.println(punktisumma2.getHetkeSkoor());
+            }
+            return true;
+        }
+        return false;
     }
 
     public boolean kasOnNõus(String vastus) {
@@ -34,7 +115,14 @@ public class MoodustaSõna {
             System.out.println("Saadi nõusolek!");
             return true;
         }
-        System.out.println("Ei ole nõusolekut.");
-        return false;
+        else if (vastus.equals("N")) {
+            System.out.println("Ei ole nõusolekut.");
+            return false;
+        }
+        System.out.print("Sisestus ei ole sobiv. Sisestage uuesti (Y/N) : ");
+        Scanner uusSisestus = new Scanner(System.in);
+        String Sisestus = uusSisestus.nextLine();
+        Sisestus = Sisestus.toUpperCase();
+        return kasOnNõus(Sisestus);
     }
 }
