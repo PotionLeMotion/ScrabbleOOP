97f3ab3ddb8a2ce11f048fdb0514cc94efa1b658
PotionLeMotion <andrerandmaa2@gmail.com>
2022-03-25 14:23:39 +0200

    midagi tehtud

--- src/AlustaMängu.java
+++ src/AlustaMängu.java
@@ -58,11 +58,11 @@ public class AlustaMängu {
         //System.out.println(mängija1Käsi);
         //System.out.println(mängija2Käsi);
 
-        // OLULINE, NO DELETE PLS
+
+
         System.out.println();
         System.out.println("Scrabble (meie moodi)");
         System.out.println();
-
         Scanner sisestusMängija1 = new Scanner(System.in);
         System.out.print("Mängija 1, sisesta oma nimi: ");
         String Mängija1 = sisestusMängija1.nextLine();
@@ -73,11 +73,43 @@ public class AlustaMängu {
         String Mängija2 = sisestusMängija2.nextLine();
         System.out.println("Sinu nimi on: " + Mängija2);
 
+        //while loop, punktid 30
         System.out.println("Mängija " + Mängija1 + " käik.");
-        System.out.println("Sinu tähed: " + mängija1Käsi);
+        System.out.println(mängija1Käsi);
+        System.out.print("Sisesta käesolevatest tähtedest sõna: ");
 
+        Scanner mängija1SõnaSisestus = new Scanner(System.in);
+        String mängija1Sõna = mängija1SõnaSisestus.nextLine();
+        mängija1Sõna = mängija1Sõna.toUpperCase();
+
+        MoodustaSõna moodustaSõna1 = new MoodustaSõna(mängija1Tähed, mängija1Sõna);
+        moodustaSõna1.MoodustaTähtedestSõna(mängija1Tähed, mängija1Sõna);
+        System.out.println();
+
+        System.out.println("Kas " + Mängija2 + " on antud sõnaga nõus? (y/n)");
+        Scanner jahVõiEi1 = new Scanner(System.in);
+        String vastus1 = jahVõiEi1.nextLine();
+        vastus1 = vastus1.toUpperCase();
+        moodustaSõna1.kasOnNõus(vastus1); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
+
+        System.out.println();
         System.out.println("Mängija " + Mängija2 + " käik.");
-        System.out.println("Sinu tähed: " + mängija2Käsi);
+        System.out.println(mängija2Käsi);
+        System.out.print("Sisesta käesolevatest tähtedest sõna: ");
+
+        Scanner mängija2SõnaSisestus = new Scanner(System.in);
+        String mängija2Sõna = mängija2SõnaSisestus.nextLine();
+        mängija2Sõna = mängija2Sõna.toUpperCase();
+
+        MoodustaSõna moodustaSõna2 = new MoodustaSõna(mängija2Tähed, mängija2Sõna);
+        moodustaSõna2.MoodustaTähtedestSõna(mängija2Tähed, mängija2Sõna);
+        System.out.println();
+
+        System.out.println("Kas " + Mängija1 + " on antud sõnaga nõus? (y/n)");
+        Scanner jahVõiEi2 = new Scanner(System.in);
+        String vastus2 = jahVõiEi2.nextLine();
+        vastus2 = vastus2.toUpperCase();
+        moodustaSõna1.kasOnNõus(vastus2); // selle true või false return põhjal peaks vaatama, kas lisada punkte ja eemaldada kasutatud tähed käest.
 
     }
 
--- src/MoodustaSõna.java
+++ src/MoodustaSõna.java
@@ -3,9 +3,38 @@ import java.util.List;
 
 public class MoodustaSõna {
 
-    List<String> tähedKäes = new ArrayList<>();
+    private ArrayList<String> tähedKäes;
+    private String sisestatudSõna;
 
-    public void MoodustaTähtedestSõna(List<String> tähedKäes) {
+    public MoodustaSõna(ArrayList<String> tähedKäes, String sisestatudSõna) {
         this.tähedKäes = tähedKäes;
+        this.sisestatudSõna = sisestatudSõna;
+    }
+
+    public void MoodustaTähtedestSõna(ArrayList<String> tähedKäes, String sisestatudSõna) {
+        ArrayList<String> tähedKäesIlmaNumbriteta = new ArrayList<>(tähedKäes);
+        for (String täht : tähedKäes) {
+            tähedKäesIlmaNumbriteta.add(täht.substring(0,1));
+        }
+        for (int i = 0; i < sisestatudSõna.length(); i++) {
+            if (tähedKäesIlmaNumbriteta.contains(sisestatudSõna.substring(i, i+1))) {
+                tähedKäesIlmaNumbriteta.remove(sisestatudSõna.substring(i, i+1));
+                if (i == sisestatudSõna.length() - 1)
+                    System.out.println("Sõna on võimalik moodustada!");
+            }
+            else {
+                System.out.println("Sinu käesolevatest tähtedest pole võimalik antud sõna moodustada.");
+                break;
+            }
+        }
+    }
+
+    public boolean kasOnNõus(String vastus) {
+        if (vastus.equals("Y")) {
+            System.out.println("Saadi nõusolek!");
+            return true;
+        }
+        System.out.println("Ei ole nõusolekut.");
+        return false;
     }
 }
