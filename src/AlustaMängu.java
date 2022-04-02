a491d379249a530852b34c870eb38cb1d89e400f
PotionLeMotion <andrerandmaa2@gmail.com>
2022-04-02 21:17:32 +0300

    Punktisumma arvutamine tuleb klassis MoodustaSõna korda teha

--- src/AlustaMängu.java
+++ src/AlustaMängu.java
@@ -21,7 +21,7 @@ public class AlustaMängu {
         punktisumma2.tähedPunktidKokku();
 
         List<String> käsi = Arrays.asList("A", "", "E", "I", "", "", "");
-        Shuffle shuffle = new Shuffle(käsi, tähestik.getTähestik());
+        //Shuffle shuffle = new Shuffle(käsi, tähestik.getTähestik());
 /*
         punktisumma1.tähedPunktidKokku();
         punktisumma2.tähedPunktidKokku();
@@ -58,8 +58,8 @@ public class AlustaMängu {
         System.out.println("Sinu nimi on: " + Mängija2);
 
         //moodustame esimesed tähed, mis on käes
-        ArrayList<String> tähed1 = mängija1Käsi.getTähedMängijale1();
-        ArrayList<String> tähed2 = mängija2Käsi.getTähedMängijale2();
+        List<String> tähed1 = mängija1Käsi.getTähedMängijale1();
+        List<String> tähed2 = mängija2Käsi.getTähedMängijale2();
 
         while (punktisumma1.getHetkeSkoor() < 30 && punktisumma2.getHetkeSkoor() < 30) {
 
@@ -73,7 +73,7 @@ public class AlustaMängu {
             String mängija1Sõna = mängija1SõnaSisestus.nextLine();
             mängija1Sõna = mängija1Sõna.toUpperCase();
 
-            MoodustaSõna moodustaSõna1 = new MoodustaSõna(mängija1Käsi.getMängija1Tähed(), mängija1Sõna);
+            MoodustaSõna moodustaSõna1 = new MoodustaSõna(mängija1Käsi.getMängija1Tähed(), mängija1Sõna, "");
             System.out.println();
 
             if (moodustaSõna1.MoodustaTähtedestSõna(mängija1Käsi.getMängija1Tähed(), mängija1Sõna, 1, punktisumma1, punktisumma2) == true) {
@@ -85,6 +85,8 @@ public class AlustaMängu {
                 int saadudSkoor = punktisumma1.antudSõnaPunktid(mängija1Sõna.toUpperCase());
                 if (!moodustaSõna1.kasOnNõus(vastus1)) punktisumma1.setHetkeSkoor(punktisumma1.getHetkeSkoor() - saadudSkoor);
                 System.out.println("Hetkeskoor on: " + punktisumma1.getHetkeSkoor());
+                Shuffle shuffle1 = new Shuffle(tähed1, tähestik.getTähestik(), moodustaSõna1.getHinnatavSõna());
+                tähed1 = shuffle1.getKäsi();
             }
 
             System.out.println();
@@ -96,7 +98,7 @@ public class AlustaMängu {
             String mängija2Sõna = mängija2SõnaSisestus.nextLine();
             mängija2Sõna = mängija2Sõna.toUpperCase();
 
-            MoodustaSõna moodustaSõna2 = new MoodustaSõna(mängija2Käsi.getMängija2Tähed(), mängija2Sõna);
+            MoodustaSõna moodustaSõna2 = new MoodustaSõna(mängija2Käsi.getMängija2Tähed(), mängija2Sõna, "");
             System.out.println();
 
             if (moodustaSõna2.MoodustaTähtedestSõna(mängija2Käsi.getMängija2Tähed(), mängija2Sõna, 2, punktisumma1, punktisumma2) == true) {
@@ -108,6 +110,8 @@ public class AlustaMängu {
                 int saadudSkoor = punktisumma2.antudSõnaPunktid(mängija2Sõna.toUpperCase());
                 if (!moodustaSõna2.kasOnNõus(vastus2)) punktisumma2.setHetkeSkoor(punktisumma2.getHetkeSkoor() - saadudSkoor);
                 System.out.println("Hetkeskoor on: " + punktisumma2.getHetkeSkoor());
+                Shuffle shuffle2 = new Shuffle(tähed2, tähestik.getTähestik(), moodustaSõna2.getHinnatavSõna());
+                tähed2 = shuffle2.getKäsi();
             }
 
         }
